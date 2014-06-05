4e49ca59d02599968259b89f0dce1776db9dd524
Rui Gomes <ruigomeseu@gmail.com>
2014-06-05 16:55:26 +0100

    added configuration

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'antigatunos',
+			'username'  => 'antigatunos',
+			'password'  => 'qqwdc9z3ciohTEMB',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
