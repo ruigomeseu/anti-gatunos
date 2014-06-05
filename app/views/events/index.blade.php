f82ca2c4b315bebe372227bfade68de3fa30ca1d
Rui Gomes <ruigomeseu@gmail.com>
2014-06-05 17:54:51 +0100

    error message in main page

--- app/views/events/index.blade.php
+++ app/views/events/index.blade.php
@@ -4,6 +4,18 @@
 
 
 <div class="container">
+    @if(Session::has('message'))
+    <div class="alert">
+        {{ Session::get('message') }}
+    </div>
+    @endif
+    @if($errors->has())
+    @foreach($errors->all() as $message)
+    <div class="alert alert-danger">
+        <p>{{ $message }}</p>
+    </div>
+    @endforeach
+    @endif
     <?php $counter = 0; ?>
     @foreach($occurrences as $occurrence)
     <ul class="timeline">
