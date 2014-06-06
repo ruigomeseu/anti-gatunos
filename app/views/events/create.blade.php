d05e559e16cce1705ca1a6e980120cba316e863c
Rui Gomes <ruigomeseu@gmail.com>
2014-06-06 11:00:13 +0100

    changed exact_address help message

--- app/views/events/create.blade.php
+++ app/views/events/create.blade.php
@@ -24,7 +24,7 @@
 
         <p>{{ Form::text('location', Input::old('location'), array("placeholder" => "Localização Aproximada", "class" => "form-control")) }}</p>
 
-        <p>{{ Form::text('exact_address', Input::old('exact_address'), array("placeholder" => "Morada Exacta (não obrigatório)", "class" => "form-control")) }}</p>
+        <p>{{ Form::text('exact_address', Input::old('exact_address'), array("placeholder" => "Morada Exacta, como aparece no Google Maps (não obrigatório)", "class" => "form-control")) }}</p>
 
         <p>{{ Form::text('sighting_time', '', array("placeholder" => "Hora", "class" => "form-control timepicker")) }}
 
